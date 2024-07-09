24e94d638d020f1b997e6d1e7770d1ec4034e5ea
creeds-knight <apedoarthur21@gmail.com>
2024-07-09 16:32:24 +0300

    task1, using async for

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,11 +3,11 @@
     Async comprehension
 """
 import asyncio
-from typing import Generator
+from typing import Iterator
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> Iteratior[float]:
     """
         Using async for in comprehensions
     """
