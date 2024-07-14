fe3713f010c0c107338e344d4d4ea969e768818b
creeds-knight <apedoarthur21@gmail.com>
2024-07-14 10:41:23 +0300

    task 11, typeVar

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+    Duck-typed annotations for safely_get_vavlue function
+"""
+from typing import Mapping, Any, Union, TypeVar
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping[Any, Any], key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
+    """
+        Returns the value for the given key if it exists, otherwise
+        return the default
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
