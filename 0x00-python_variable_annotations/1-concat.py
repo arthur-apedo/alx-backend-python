950a37361968b04f6052e89c1e363517bda93a44
creeds-knight <apedoarthur21@gmail.com>
2024-07-14 08:07:59 +0300

    task1, concatinating strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    Basic annotations -> concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+        returns two concated strings
+    """
+    return str1 + str2
