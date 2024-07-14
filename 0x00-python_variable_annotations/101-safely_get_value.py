e5ae2232846fd924dd9a33f76f52b90156b3a4e1
creeds-knight <apedoarthur21@gmail.com>
2024-07-14 10:47:18 +0300

    task 11, typeVar

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -2,12 +2,12 @@
 """
     Duck-typed annotations for safely_get_vavlue function
 """
-from typing import Mapping, Any, Union, TypeVar
+from typing import MutableMapping, Any, Union, TypeVar
 
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping[Any, Any], key: Any,
+def safely_get_value(dct: MutableMapping[Any, Any], key: Any,
                      default: Union[T, None] = None) -> Union[Any, T]:
     """
         Returns the value for the given key if it exists, otherwise
