72190e17ace3d3aa55947c80cdac1b2d3e771145
creeds-knight <apedoarthur21@gmail.com>
2024-07-14 08:58:36 +0300

    task7, tuple

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -2,7 +2,7 @@
 """
     Complex types -> string and int/float to tuple
 """
-from typing import Tuple
+from typing import Tuple, Union
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
